5e5d3696fd86916ece5d9dd30bbc12a824226a98
static <kmc7468@naver.com>
2020-08-27 21:58:21 +0900

    A~C번 예제 코드 들여쓰기 스페이스로 변경

--- devroom-ps1/a-what-the-triangle-is/example_solution.cpp
+++ devroom-ps1/a-what-the-triangle-is/example_solution.cpp
@@ -3,31 +3,31 @@
 #include <iterator>
 
 int main() {
-	int lengths[3];
-	for (int& l : lengths) {
-		std::cin >> l;
-	}
-	std::sort(std::begin(lengths), std::end(lengths));
+    int lengths[3];
+    for (int& l : lengths) {
+        std::cin >> l;
+    }
+    std::sort(std::begin(lengths), std::end(lengths));
 
-	const bool isIsosceles = lengths[0] == lengths[1] || lengths[1] == lengths[2];
-	const bool isEquilateral = lengths[0] == lengths[2];
-	if (isEquilateral) {
-		std::cout << "Equilateral";
-		return 0;
-	}
+    const bool isIsosceles = lengths[0] == lengths[1] || lengths[1] == lengths[2];
+    const bool isEquilateral = lengths[0] == lengths[2];
+    if (isEquilateral) {
+        std::cout << "Equilateral";
+        return 0;
+    }
 
-	const int abSquared = lengths[0] * lengths[0] + lengths[1] * lengths[1];
-	const int cSquared = lengths[2] * lengths[2];
-	if (abSquared == cSquared) {
-		std::cout << "Right";
-	} else if (abSquared > cSquared) {
-		std::cout << "Acute";
-	} else {
-		std::cout << "Obtuse";
-	}
+    const int abSquared = lengths[0] * lengths[0] + lengths[1] * lengths[1];
+    const int cSquared = lengths[2] * lengths[2];
+    if (abSquared == cSquared) {
+        std::cout << "Right";
+    } else if (abSquared > cSquared) {
+        std::cout << "Acute";
+    } else {
+        std::cout << "Obtuse";
+    }
 
-	if (isIsosceles) {
-		std::cout << " isosceles";
-	}
-	std::cout << " triangle";
+    if (isIsosceles) {
+        std::cout << " isosceles";
+    }
+    std::cout << " triangle";
 }
--- devroom-ps1/b-time-to-change-the-phone/example_solution.cpp
+++ devroom-ps1/b-time-to-change-the-phone/example_solution.cpp
@@ -3,51 +3,51 @@
 #include <vector>
 
 struct Phone {
-	std::string Name;
-	std::string Maker;
-	int Price;
-	int Gongsi;
-	int Performance;
+    std::string Name;
+    std::string Maker;
+    int Price;
+    int Gongsi;
+    int Performance;
 
-	bool IsGongsi;
-	double Ratio;
+    bool IsGongsi;
+    double Ratio;
 };
 
 int main() {
-	int t;
-	std::cin >> t;
+    int t;
+    std::cin >> t;
 
-	std::vector<Phone> phones(t);
-	for (int i = 0; i < t; ++i) {
-		Phone& phone = phones[i];
-		std::cin >> phone.Name >> phone.Maker >> phone.Price >> phone.Gongsi >> phone.Performance;
+    std::vector<Phone> phones(t);
+    for (int i = 0; i < t; ++i) {
+        Phone& phone = phones[i];
+        std::cin >> phone.Name >> phone.Maker >> phone.Price >> phone.Gongsi >> phone.Performance;
 
-		const double gongsiRatio = static_cast<double>(phone.Performance) / (phone.Price - phone.Gongsi);
-		const double suntaekRatio = phone.Performance / (0.75 * phone.Price);
-		if ((phone.IsGongsi = gongsiRatio > suntaekRatio)) {
-			phone.Price -= phone.Gongsi;
-		} else {
-			phone.Price *= 0.75;
-		}
-		phone.Ratio = static_cast<double>(phone.Performance) / phone.Price;
-	}
+        const double gongsiRatio = static_cast<double>(phone.Performance) / (phone.Price - phone.Gongsi);
+        const double suntaekRatio = phone.Performance / (0.75 * phone.Price);
+        if ((phone.IsGongsi = gongsiRatio > suntaekRatio)) {
+            phone.Price -= phone.Gongsi;
+        } else {
+            phone.Price *= 0.75;
+        }
+        phone.Ratio = static_cast<double>(phone.Performance) / phone.Price;
+    }
 
-	int max;
-	std::string want;
-	std::cin >> max >> want;
+    int max;
+    std::string want;
+    std::cin >> max >> want;
 
-	Phone* best = nullptr;
-	for (Phone& phone : phones) {
-		if (phone.Maker != want || phone.Price > max) continue;
-		else if (best && best->Ratio < phone.Ratio || !best) {
-			best = &phone;
-		}
-	}
+    Phone* best = nullptr;
+    for (Phone& phone : phones) {
+        if (phone.Maker != want || phone.Price > max) continue;
+        else if (best && best->Ratio < phone.Ratio || !best) {
+            best = &phone;
+        }
+    }
 
-	std::cout << best->Name << '\n';
-	if (best->IsGongsi) {
-		std::cout << "Gongsi";
-	} else {
-		std::cout << "Suntaek";
-	}
+    std::cout << best->Name << '\n';
+    if (best->IsGongsi) {
+        std::cout << "Gongsi";
+    } else {
+        std::cout << "Suntaek";
+    }
 }
